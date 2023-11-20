64d8d6262ee360d5bce6eb6147ed62a12f2edf0d
choihyeonji00 <151499430+choihyeonji00@users.noreply.github.com>
2023-11-20 18:58:23 +0900

    안드로이드 6월 12일 과제
    6월 12일 과제

---
+++ app/src/main/java/com/example/b2101029_0612/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.b2101029_0612;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gv = (GridView) findViewById(R.id.gridView);
+        MyGridAdapter gridAdapter = new MyGridAdapter(this);
+        gv.setAdapter(gridAdapter);
+    }
+
+    public class MyGridAdapter extends BaseAdapter {
+        Context context;
+        Integer[] posterID = {R.drawable.mov01, R.drawable.mov02, R.drawable.mov03, R.drawable.mov04, R.drawable.mov05,
+                R.drawable.mov06, R.drawable.mov07, R.drawable.mov08, R.drawable.mov09, R.drawable.mov10,
+                R.drawable.mov11, R.drawable.mov12, R.drawable.mov13, R.drawable.mov14, R.drawable.mov15,
+                R.drawable.mov16, R.drawable.mov17, R.drawable.mov18, R.drawable.mov19, R.drawable.mov20};
+
+        public MyGridAdapter(Context c) {
+            context = c;
+        }
+
+        @Override
+        public int getCount() {
+            return posterID.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup viewGroup) {
+            ImageView imageView = new ImageView(context);
+            imageView.setLayoutParams(new ViewGroup.LayoutParams(200, 300));
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imageView.setPadding(5, 5, 5, 5);
+            imageView.setImageResource(posterID[position]);
+
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    View dialogView = (View) View.inflate(MainActivity.this, R.layout.dailog, null);
+                    AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                    ImageView ivPoster = (ImageView) dialogView.findViewById(R.id.imageView);
+                    ivPoster.setImageResource(posterID[position]);
+                    dlg.setTitle("큰 포스터");
+                    dlg.setView(dialogView);
+                    dlg.setNegativeButton("닫기", null);
+                    dlg.show();
+                }
+            });
+            return imageView;
+        }
+    }
+}
